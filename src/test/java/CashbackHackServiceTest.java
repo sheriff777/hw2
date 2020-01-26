4f363b3a3b9ff740d2b6cd9dc8ba316a482b98d1
sheriff777 <aibek.syd@gmail.com>
2020-01-26 21:19:23 +0600

    Second commit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,28 +1,48 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturnTrueIfAmountls2900() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    void shouldCalculateCashback(int amount, int expected, String message) {
         CashbackHackService service = new CashbackHackService();
-        int amount = 2900;
-
-        int actual = service.remain(amount);
-        int expected = 100;
-
-        assertEquals(expected, actual);
+        int actual = CashbackHackService.remain(amount);
+        assertEquals(expected, actual, message);
     }
 
+
     @Test
-    void shouldReturnFalseIfAmountls1000() {
+    void shouldGiveMassageIfAmountLessThanZero(){
         CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-
-        int actual = service.remain(amount);
-        int expected = 0;
-
-        assertEquals(expected, actual);
+        int amount = -10;
+        assertThrows(IllegalArgumentException.class, ()-> service.remain(amount) );
     }
+
+
+//    @Test
+//    void shouldReturnTrueIfAmountls2900() {
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 2900;
+//
+//        int actual = service.remain(amount);
+//        int expected = 100;
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void shouldReturnFalseIfAmountls1000() {
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 1000;
+//
+//        int actual = service.remain(amount);
+//        int expected = 0;
+//
+//        assertEquals(expected, actual);
+//    }
 }
